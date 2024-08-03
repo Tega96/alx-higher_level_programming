0436de30b325de8a4bc2059a2731ae00d80f1078
Tega96 <otiteotega@gmail.com>
2024-08-03 07:59:58 -0700

    print lower case

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 7-islower.py
+# Otite Otega <Tega96@github.com>
+
+"""This code checks if an alphabet is lower case"""
+def islower(c):
+    if org(c) >= 97 or org(c) <= 122:
+        return True
+    else:
+        return False
