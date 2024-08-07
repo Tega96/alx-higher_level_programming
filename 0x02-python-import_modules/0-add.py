8bacbdd3d5ed2bc3530b8a3dea5e151fda48dcdc
Tega96 <otiteotega@gmail.com>
2024-08-07 15:13:44 -0700

    add a and b

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-add.py
+# Otega Otite <Tega96@github.com>
+
+from add_0 import add
+
+if __name__== "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
