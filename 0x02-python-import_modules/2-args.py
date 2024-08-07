80dfe549cac08df6c36b4b39a9fe823476d20cd6
Tega96 <otiteotega@gmail.com>
2024-08-07 16:52:59 -0700

    command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+
+    if l == 0:
+        print("{} arguments.".format(l))
+    elif l == 1:
+        print("{} arguments.".format(l))
+    else:
+        print("{} arguments.".format(l))
+
+    if l >= 1:
+        for l, arg in enumerate()sys.argv[1:], 1):
+            if l != 0:
+                print("{}: {}".format(l, arg))
