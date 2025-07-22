f6ab7e8cb44af75f6c2a493004897f9c8aeb4925
Tega96 <otiteotega@gmail.com>
2025-07-22 07:22:59 +0100

    value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+//Script to print the first argument
+const [args] = process.argv.slice(2);
+
+if (args === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args);
+}
