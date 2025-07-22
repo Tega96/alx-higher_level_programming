d5f799aefaec9f8986f3285034b6ad5fc604534e
Tega96 <otiteotega@gmail.com>
2025-07-22 06:54:31 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,9 +5,9 @@ const argv = process.argv.slice(2);
 
 // print process.arg
 if (argv.length === 0) {
-	console.log('No argument');
-}else if (argv.length === 1) {
-	console.log('Argument found');
-}else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
