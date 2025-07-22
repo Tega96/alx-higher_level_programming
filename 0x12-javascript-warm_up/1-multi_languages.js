95753a79e55eabb4a7081f2fca67769e4f4af785
Tega96 <otiteotega@gmail.com>
2025-07-22 05:52:22 +0100

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Script to print 3 lines
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
+console.log(a);
+console.log(b);
+console.log(c);
