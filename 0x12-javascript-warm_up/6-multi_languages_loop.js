25e7be057cb30f5ac87db082e4b620d79e03a3ed
Tega96 <otiteotega@gmail.com>
2025-07-23 03:11:46 +0100

    loop to language

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Script to loop thourgh an array
+
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let  i = 0;
+while (i < lang.length) {
+  console.log(lang[i]);
+  i++;
+}
